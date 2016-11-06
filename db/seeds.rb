faf353c5a47b549eb7277b1515f0bd0bd30b1386
Chip Irek <chip.irek@gmail.com>
2016-11-06 08:57:37 -0500

    Updating static content and pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,18 @@
+Todo.destroy_all
+User.destroy_all
+
 # Users
+
+print 'Creating users...'
+
+User.create!(name:  "Chip Irek",
+             email: "chip.irek@gmail.com",
+             password:              "lollip0p",
+             password_confirmation: "lollip0p",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -19,3 +33,18 @@ User.create!(name:  "Example User",
                activated_at: Time.zone.now)
 end
 
+# Todos
+
+print 'Creating new todos...'
+
+Todo.create(:subject=>'1 due today',:due_date=>Date.today,:recurrence=>0, :user_id=>User.first.id)
+Todo.create(:subject=>'2 completed today',:due_date=>Date.today,:recurrence=>0, :is_complete=>true, :user_id=>User.first.id)
+Todo.create(:subject=>'9 due yesterday',:due_date=>Time.now.to_date.advance(:days=>-1).advance(:hours=>-5).strftime("%m/%d/%Y"),:recurrence=>0, :user_id=>User.first.id)
+Todo.create(:subject=>'due tomorrow',:due_date=>Date.tomorrow,:recurrence=>0, :user_id=>User.first.id)
+Todo.create(:subject=>'item with NO due date',:due_date=>nil,:recurrence=>0, :user_id=>User.first.id)
+Todo.create(:subject=>'daily recurrence',:due_date=>Date.today,:recurrence=>1, :user_id=>User.first.id)
+Todo.create(:subject=>"Chip should NOT see Hartl's items #1",:due_date=>Date.today,:recurrence=>1, :user_id=>User.last.id)
+Todo.create(:subject=>"Chip should NOT see Hartl's items #2",:due_date=>Date.tomorrow,:recurrence=>1, :user_id=>User.last.id)
+sleep(2)
+print 'done.'
+puts 
